f200321ac76689cd29538a41df8b40f92b3102c1
Stuart Pook <stuart12@users.noreply.github.com>
2018-04-29 18:19:36 +0200

    start of check script

---
+++ containers/check.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+check(){
+    h=$1
+    p=${2-https}
+    s=${3+-starttls $3}
+    echo | openssl s_client -servername $h.pook.it -connect $h.pook.it:$p $s 2>/dev/null | openssl x509 -noout -dates | sed -e "s/^/$1: /"
+}
+check nextcloud
+check hh
+check ffsync
+check smtp3 submission smtp
+check imap3 imap imap
