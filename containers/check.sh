e74492f7f1b4c8dc8d151a1a5f07992680655322
Stuart Pook <stuart12@users.noreply.github.com>
2019-05-13 12:16:31 +0000

    print date of last letsencrypt key update

--- containers/check.sh
+++ containers/check.sh
@@ -12,3 +12,4 @@ check smtp3 submission smtp
 check imap3 imap imap
 
 # use https://github.com/ahaw021/SSL-MAIL-PROTOCOLS-TESTING ?
+docker run --rm --mount type=volume,source=letsencrypt-etc,destination=/etc/letsencrypt,readonly  stuart/debian-sid:latest ls -Ll /etc/letsencrypt/live/hh.pook.it/fullchain.pem
